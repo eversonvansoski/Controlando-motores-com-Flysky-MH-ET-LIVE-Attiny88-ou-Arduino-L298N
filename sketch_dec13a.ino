5b19ffc0add71dee8442fcd14ae5ebdbf84c94aa
Everson Vansoski <eversonvansoski@gmail.com>
2023-12-14 22:26:26 -0300

    programa finalizado

--- sketch_dec13a.ino
+++ sketch_dec13a.ino
@@ -1,15 +1,4 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(3,INPUT);
-  pinMode(5,INPUT);
-
-
-  pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-
-}
-
+//ranges dos valores dos canais 1 e 2
 int minEsquerda = 998;
 int maxEsquerda = 1400;
 int minDireita = 1550;
@@ -17,80 +6,98 @@ int maxDireita = 1970;
 
 int minFrente = 1550;
 int maxFrente = 1988;
-int minTras = 998;
+int minTras = 990;
 int maxTras = 1420;
 
+int ch1Pin = A0;
+int ch2Pin = A1;
+int velocidadePin = 6;
+int motorEsquerdoFrentePin = 2;
+int motorEsquerdoTrasPin = 3;
+int motorDireitoFrentePin = 4;
+int motorDireitoTrasPin = 5;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ch1Pin,INPUT);
+  pinMode(ch2Pin,INPUT);
+
+  pinMode(velocidadePin, OUTPUT);
+  pinMode(motorEsquerdoFrentePin, OUTPUT);
+  pinMode(motorEsquerdoTrasPin, OUTPUT);
+  pinMode(motorDireitoFrentePin, OUTPUT);
+  pinMode(motorDireitoTrasPin, OUTPUT);
+}
+
+
+
 void loop() {
-double ch1 = pulseIn(3,HIGH);
-double ch2 = pulseIn(5,HIGH);
+double ch1 = pulseIn(ch1Pin,HIGH);
+double ch2 = pulseIn(ch2Pin,HIGH);
 
 
-//Serial.print("ch3: ");
-//Serial.print(ch3);
+//prints para identificar os ranges de cada canal
+//Serial.print("ch1: ");
+///Serial.print(ch1);
 //Serial.print("\t");
-//Serial.print("velocidade: ");
-//Serial.print(velocidade(ch2));
-//Serial.print("\t");
-//Serial.print("analogicoPraFrente: ");
-//Serial.print(analogicoPraFrente(ch2));
-//Serial.print("\t");
-//Serial.print("analogicoPraTras: ");
-//Serial.print(analogicoPraTras(ch2));
-//Serial.print("\t");
-//Serial.print("analogicoPraEsquerda: ");
-//Serial.print(analogicoPraEsquerda(ch1));
-//Serial.print("\t");
-//Serial.print("analogicoPraDireita: ");
-//Serial.print(analogicoPraDireita(ch1));
-//Serial.println("");
-analogWrite(11, 0);
-digitalWrite(12, LOW);
-digitalWrite(13, LOW);
+//Serial.print("ch2: ");
+Serial.print(velocidade(ch2));
+Serial.println("");
+
+analogWrite(velocidadePin, 0);
+digitalWrite(motorEsquerdoFrentePin, LOW);
+digitalWrite(motorEsquerdoTrasPin, LOW);
+digitalWrite(motorDireitoFrentePin, LOW);
+digitalWrite(motorDireitoTrasPin, LOW);
 
 if(analogicoPraFrente(ch2)){
+  analogWrite(velocidadePin, velocidade(ch2));
+
   if(!analogicoPraEsquerda(ch1) && !analogicoPraDireita(ch1)){
-    Serial.print(velocidade(ch2));
-    Serial.println("");
-    analogWrite(11, velocidade(ch2));
-    digitalWrite(12, HIGH);
-    digitalWrite(13, LOW);
+    //Serial.print("pra frente"); 
+    digitalWrite(motorEsquerdoFrentePin, HIGH);
+    digitalWrite(motorDireitoFrentePin, HIGH);
   }
   else if(analogicoPraDireita(ch1)){
-    Serial.print("pra frente à direita");
-    Serial.println("");
+    //Serial.print("pra frente à direita");
+    digitalWrite(motorEsquerdoFrentePin, HIGH);
   }
   else{
-    Serial.print("pra frente à esquerda");
-    Serial.println("");
+    //Serial.print("pra frente à esquerda");
+    digitalWrite(motorDireitoFrentePin, HIGH);
   }
 }
 
 else if(analogicoPraTras(ch2)){
+  analogWrite(velocidadePin, velocidade(ch2));
+
   if(!analogicoPraEsquerda(ch1) && !analogicoPraDireita(ch1)){
-    Serial.print(velocidade(ch2));
-    Serial.println("");
-    analogWrite(11, velocidade(ch2));
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
+    //Serial.print("pra tras");
+    digitalWrite(motorEsquerdoTrasPin, HIGH);
+    digitalWrite(motorDireitoTrasPin, HIGH);
   }
   else if(analogicoPraDireita(ch1)){
-    Serial.print("pra tras à direita");
-    Serial.println("");
+    //Serial.print("pra tras à direita");
+    digitalWrite(motorEsquerdoTrasPin, HIGH);
   }
   else{
-    Serial.print("pra tras à esquerda");
-    Serial.println("");
+    //Serial.print("pra tras à esquerda");
+    digitalWrite(motorDireitoTrasPin, HIGH);
   }
 }
 
 else {
+  analogWrite(velocidadePin, 120);
+
   if(analogicoPraDireita(ch1)){
-    Serial.print("giro para direita");
-    Serial.println("");
+    //Serial.print("giro para direita");
+    digitalWrite(motorEsquerdoFrentePin, HIGH);
+    digitalWrite(motorDireitoTrasPin, HIGH);
   }
   else if(analogicoPraEsquerda(ch1)){
-    Serial.print("giro para esquerda");
-    Serial.println("");
+    //Serial.print("giro para esquerda");
+    digitalWrite(motorEsquerdoTrasPin, HIGH);
+    digitalWrite(motorDireitoFrentePin, HIGH);
   }
 }
 
