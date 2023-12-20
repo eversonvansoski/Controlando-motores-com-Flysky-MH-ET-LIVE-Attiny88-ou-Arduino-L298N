3ec139db815dc64c28b0a066d96b60dfed26ab92
Everson Vansoski <eversonvansoski@gmail.com>
2023-12-20 01:01:38 -0300

    adaptando o código para o attiny88

--- sketch_dec13a.ino
+++ sketch_dec13a.ino
@@ -1,26 +1,27 @@
 //ranges dos valores dos canais 1 e 2
-int minEsquerda = 998;
-int maxEsquerda = 1400;
-int minDireita = 1550;
-int maxDireita = 1970;
+//comentados e utilizados nas funções no final do código para economia de memória, necessário pela limitação do MH-ET LIVE Tiny88
+//caso utilize arduino, pode-se utilizar das variáveis globais, pois possui maior capacidade
 
-int minFrente = 1550;
-int maxFrente = 1988;
-int minTras = 990;
-int maxTras = 1420;
+//int minEsquerda = 998;
+//int maxEsquerda = 1400;
+//int minDireita = 1550;
+//int maxDireita = 1970;
+//int minFrente = 1550;
+//int maxFrente = 1988;
+//int minTras = 990;
+//int maxTras = 1420;
 
-int ch1Pin = A0;
-int ch2Pin = A1;
-int velocidadePin = 6;
-int motorEsquerdoFrentePin = 2;
-int motorEsquerdoTrasPin = 3;
-int motorDireitoFrentePin = 4;
-int motorDireitoTrasPin = 5;
+
+int motorEsquerdoFrentePin = 5;
+int motorEsquerdoTrasPin = 6;
+int motorDireitoFrentePin = 7;
+int motorDireitoTrasPin = 8;
+int velocidadePin = 9;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ch1Pin,INPUT);
-  pinMode(ch2Pin,INPUT);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
 
   pinMode(velocidadePin, OUTPUT);
   pinMode(motorEsquerdoFrentePin, OUTPUT);
@@ -32,17 +33,13 @@ void setup() {
 
 
 void loop() {
-double ch1 = pulseIn(ch1Pin,HIGH);
-double ch2 = pulseIn(ch2Pin,HIGH);
+double ch1 = pulseIn(A0,HIGH);
+double ch2 = pulseIn(A1,HIGH);
 
 
 //prints para identificar os ranges de cada canal
 //Serial.print("ch1: ");
-///Serial.print(ch1);
-//Serial.print("\t");
-//Serial.print("ch2: ");
-Serial.print(velocidade(ch2));
-Serial.println("");
+//Serial.print(ch1);
 
 analogWrite(velocidadePin, 0);
 digitalWrite(motorEsquerdoFrentePin, LOW);
@@ -66,6 +63,7 @@ if(analogicoPraFrente(ch2)){
     //Serial.print("pra frente à esquerda");
     digitalWrite(motorDireitoFrentePin, HIGH);
   }
+  //Serial.println("");
 }
 
 else if(analogicoPraTras(ch2)){
@@ -84,10 +82,11 @@ else if(analogicoPraTras(ch2)){
     //Serial.print("pra tras à esquerda");
     digitalWrite(motorDireitoTrasPin, HIGH);
   }
+  //Serial.println("");
 }
 
 else {
-  analogWrite(velocidadePin, 120);
+  analogWrite(velocidadePin, 80);
 
   if(analogicoPraDireita(ch1)){
     //Serial.print("giro para direita");
@@ -99,36 +98,43 @@ else {
     digitalWrite(motorEsquerdoTrasPin, HIGH);
     digitalWrite(motorDireitoFrentePin, HIGH);
   }
+  //Serial.println("");
 }
 
 }
 
 int velocidade(double ch) {
   int velocidade = 0;
-  int velocidadeFrente = map(ch, minFrente, maxFrente, 0, 255);   
-  int velocidadeTras = map((maxTras + minTras) - ch, minTras, maxTras, 0, 255); 
+  if (ch > 0){
+    int velocidadeFrente = map(ch, 1550, 1988, 0, 255);   
+    int velocidadeTras = map((1420 + 990) - ch, 990, 1420, 0, 255); 
 
-  if (velocidadeFrente > 0 || velocidadeTras > 0){
-    if(velocidadeFrente > velocidadeTras){
-      velocidade = velocidadeFrente;
+    if (velocidadeFrente > 0 || velocidadeTras > 0){
+      if(velocidadeFrente > velocidadeTras){
+        velocidade = velocidadeFrente;
+      }
+      else{
+        velocidade = velocidadeTras;
+      }
     }
-    else{
-      velocidade = velocidadeTras;
-    }
+  }
+  if (velocidade > 3 && velocidade < 60){
+    velocidade = 60;
   }
   return velocidade;
 }
+
 bool analogicoPraFrente(double ch){
-  return ch >= minFrente;
+  return ch >= 1550; //valor minimo do analógico para frente
 }
 bool analogicoPraTras(double ch){
-  return ch <= maxTras;
+  return ch <= 1420; //valor maximo do analógico para tras
 }
 bool analogicoPraEsquerda(double ch){
-  return ch <= maxEsquerda;
+  return ch <= 1400; //valor maximo do analógico para a esquerda
 }
 bool analogicoPraDireita(double ch){
-  return ch >= minDireita;
+  return ch >= 1550; //valor minimo do analógico para a direita
 }
 
 
